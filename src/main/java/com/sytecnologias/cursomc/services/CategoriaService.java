6afe3b9c2ee5eea99eed4ed202a9ee3d0c886d20
Yuri Jivago <yurijivagoads@gmail.com>
2021-06-13 07:45:26 -0300

    PAjustes no endpoit /cateegorias/{id}

--- src/main/java/com/sytecnologias/cursomc/services/CategoriaService.java
+++ src/main/java/com/sytecnologias/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.sytecnologias.cursomc.domain.Categoria;
 import com.sytecnologias.cursomc.repositories.CategoriaRepository;
+import com.sytecnologias.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -15,7 +16,14 @@ public class CategoriaService {
 	private CategoriaRepository repo;
 	
 	public Categoria buscar(Integer id) {
+		/*
+		 * código antigo, antes do tratamento de exceção
+		 * 
+	 	   Optional<Categoria> obj = repo.findById(id);
+		   return obj.orElse(null);
+		*/
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
