083ecf553f2c4926a33009b6e7a5ef567fbdac67
Yuri Jivago <yurijivagoads@gmail.com>
2021-06-25 20:16:34 -0300

    Endpont /clientes/{id} disponivel

---
+++ src/main/java/com/sytecnologias/cursomc/services/ClienteService.java
@@ -0,0 +1,29 @@
+package com.sytecnologias.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sytecnologias.cursomc.domain.Cliente;
+import com.sytecnologias.cursomc.repositories.ClienteRepository;
+import com.sytecnologias.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		/*
+		 * código antigo, antes do tratamento de exceção
+		 * 
+	 	   Optional<Cliente> obj = repo.findById(id);
+		   return obj.orElse(null);
+		*/
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
