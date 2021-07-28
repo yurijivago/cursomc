09ff5ee60ae6b5541533f16517868aa9ef24d74f
Yuri Jivago <yurijivagoads@gmail.com>
2021-07-28 00:26:00 -0300

    Listando todas categorias com DTO

--- src/main/java/com/sytecnologias/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/sytecnologias/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,9 @@
 package com.sytecnologias.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.sytecnologias.cursomc.domain.Categoria;
+import com.sytecnologias.cursomc.dto.CategoriaDTO;
 import com.sytecnologias.cursomc.services.CategoriaService;
 
 @RestController
@@ -46,6 +50,14 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj ->  new CategoriaDTO(obj))
+				.collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 	
 	
--- src/main/java/com/sytecnologias/cursomc/services/CategoriaService.java
+++ src/main/java/com/sytecnologias/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.sytecnologias.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,6 +47,9 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma cateroria que possui produtos");
 		}
-		
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 }
