7d1d4410ebc1a094067be83045cf708c4600598d
Yuri Jivago <yurijivagoads@gmail.com>
2021-07-29 00:52:35 -0300

    Correções: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/sytecnologias/cursomc/services/CategoriaService.java
+++ src/main/java/com/sytecnologias/cursomc/services/CategoriaService.java
@@ -40,8 +40,9 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 	
 	public void delete(Integer id) {
@@ -64,5 +65,9 @@ public class CategoriaService {
 	
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 }
