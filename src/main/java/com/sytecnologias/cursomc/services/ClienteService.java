fe82f2ede33c3f6d9463393c23692ca24de53226
Yuri Jivago <yurijivagoads@gmail.com>
2021-07-29 01:33:13 -0300

    Ajustando e testando a delecao de Cliente

--- src/main/java/com/sytecnologias/cursomc/services/ClienteService.java
+++ src/main/java/com/sytecnologias/cursomc/services/ClienteService.java
@@ -56,7 +56,7 @@ public class ClienteService {
 		try {
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir uporque há entidades relacionadas");
+			throw new DataIntegrityException("Não é possível excluir uporque há pedidos relacionados");
 		}
 	}
 	
